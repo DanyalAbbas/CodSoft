6e9df5295a7a110163c3cc0633c0ae0fa0823dc3
Danyal Abbas <abasdaniel61@gmail.com>
2024-02-27 12:11:37 +0500

    Create task_3.py

---
+++ task_3.py
@@ -0,0 +1,20 @@
+import random
+
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', '\\', ':', ';', '<', '>', ',', '.', '?', '/']
+
+all_characters = alphabets + nums + special
+
+
+length = int(input("How long do you want your password to be? : "))
+password = ""
+for i in range(length):
+    password += random.choice(all_characters)
+
+print(f"Generated password : {password}")
+
+
